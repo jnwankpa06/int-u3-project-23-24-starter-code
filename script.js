80593f07b7fd12431b158dae2844dc5e41188c18
J <149433971+jnwankpa06@users.noreply.github.com>
2024-03-27 21:28:10 +0000

    Finished last class

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 // Declare variables below to save the different divs of your story.
 
+let storyOpening = document.querySelector('.story-opening'); 
+
+let optionOneScreen = document.querySelector('.option-one-screen');
+let optionOneButton = document.querySelector('.option-one');
+
+let directionOneButton = document.querySelector('.direction-one')
+let directionTwoButton = document.querySelector('.direction-two')
+
+let optionTwoScreen = document.querySelector('.option-two-screen');
+let optionTwoButton = document.querySelector('.option-two');
+let optionTwoEnd = document.querySelector('.option-two-end');
+let homeOptionButton = document.querySelector('.home-opt-button');
+
+let optionThreeScreen = document.querySelector('.option-three-screen');
+let optionThreeButton = document.querySelector('.option-three');
+let optionThreeEnd = document.querySelector('.option-three-end');
+
+let optionFourButton = document.querySelector('.option-four');
+let optionFourEnd = document.querySelector('.option-four-end'); 
+  
 
 
 
@@ -12,15 +32,57 @@
 //  - Then fill in the blanks with the correct variables.
 
 
-// INSERT_VARIABLE.addEventListener('click', function(){
+optionOneButton.addEventListener('click', function(){
+  storyOpening.style.display = 'none';  // hide opening screen
+  optionOneButton.style.display = 'none'; // hide opt 1 btn
+  optionTwoButton.style.display = 'none'; // hide opt 2 btn
+  optionOneScreen.style.display = 'block';  // show opt one screen
+  directionOneButton.style.display = 'block'; // show right direction btn
+  directionTwoButton.style.display = 'block'; // show left direction btn
+});
 
-// });
+optionTwoButton.addEventListener('click', function(){
+  storyOpening.style.display = 'none';  // hide opening screen
+  optionTwoScreen.style.display = 'block';  // show option two screen
+  optionOneButton.style.display = 'none'; //hide opt 1 btn
+  optionTwoButton.style.display = 'none'; //hide opt 2 btn
+  homeOptionButton.style.display = 'block'; //show
+ });
 
-// INSERT_VARIABLE.addEventListener('click', function(){
+  directionOneButton.addEventListener('click', function(){
+    optionOneScreen.style.display = 'none'; // hide opt 1 screen
+    optionThreeScreen.style.display = 'block'; // show opt 3 screen
+    directionOneButton.style.display = 'none'; // hide yes btn
+    directionTwoButton.style.display = 'none'; // hide turn back btn
+    optionThreeButton.style.display = 'block'; // show opt 3 btn
+    optionFourButton.style.display = 'block'; // show opt 4 btn
+  })
 
-// });
+  directionTwoButton.addEventListener('click', function(){
+    directionTwoButton.style.display = 'none' //hide
+    directionOneButton.style.display = 'none' //hide
+    optionOneScreen.style.display = 'none' //hide
+    optionTwoScreen.style.display = 'block'; //show
+    optionTwoButton.style.display = 'none'; //hide
+    homeOptionButton.style.display = 'block'; //show
+  })
 
+  homeOptionButton.addEventListener('click', function(){
+    homeOptionButton.style.display = 'none'; //hide
+    optionTwoEnd.style.display = 'block'; //show
+    optionTwoScreen.style.display = 'none'; //hide
+  })
 
-// INSERT_VARIABLE.addEventListener('click', function(){
+optionThreeButton.addEventListener('click', function(){
+  optionThreeScreen.style.display = 'none'; // hide opt three screen
+  optionThreeEnd.style.display = 'block'; // show opt three ending
+  optionThreeButton.style.display = 'none'; // hide opt btn
+  optionFourButton.style.display = 'none' // hide run away btn
+});
 
-// });
+optionFourButton.addEventListener('click', function(){
+  optionThreeScreen.style.display = 'none'; //hide option three screen
+  optionFourEnd.style.display = 'block'; //show option four ending
+  optionFourButton.style.display = 'none'; //hide
+  optionThreeButton.style.display = 'none'; //hide
+ });
